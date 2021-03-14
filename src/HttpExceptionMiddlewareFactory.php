407dd46835a53045dbfb0642d05148067ed23072
Jonathan Maron <jonathan.maron@theimagingsource.com>
2021-03-14 05:56:55 +0100

    Updated factory

--- src/HttpExceptionMiddlewareFactory.php
+++ src/HttpExceptionMiddlewareFactory.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Ctw\Middleware\HttpExceptionMiddleware;
 
+use Mezzio\Template\TemplateRendererInterface as Template;
 use Mezzio\LaminasView\LaminasViewRenderer as TemplateRenderer;
 use Psr\Container\ContainerInterface;
 
@@ -10,7 +11,7 @@ class HttpExceptionMiddlewareFactory
 {
     public function __invoke(ContainerInterface $container): HttpExceptionMiddleware
     {
-        $template = $container->get(TemplateRenderer::class);
+        $template = $container->get(Template::class);
         //$template = $container->get('ctw_template_renderer');
 
         $middleware = new HttpExceptionMiddleware();
