45cc41d10d7fdbfc1eb97b5ce2ba08b229022195
Jonathan Maron <jonathan.maron@theimagingsource.com>
2021-03-16 06:33:38 +0100

    Added tests

--- test/HttpExceptionMiddlewareTest.php
+++ test/HttpExceptionMiddlewareTest.php
@@ -3,14 +3,69 @@ declare(strict_types=1);
 
 namespace CtwTest\Middleware\HttpExceptionMiddleware;
 
+use Ctw\Http\HttpException;
+use Ctw\Http\HttpStatus;
 use Ctw\Middleware\HttpExceptionMiddleware\HttpExceptionMiddleware;
+use Mezzio\LaminasView\LaminasViewRenderer as TemplateRenderer;
 use Middlewares\Utils\Dispatcher;
-use Middlewares\Utils\Factory;
+use Psr\Http\Message\ResponseInterface;
+use stdClass;
 
 class HttpExceptionMiddlewareTest extends AbstractCase
 {
     public function testHttpExceptionMiddleware(): void
     {
-        $this->assertTrue(true);
+        $message = hash('sha256', (string) microtime(true));
+
+        $template  = new TemplateRenderer();
+        $path      = realpath(__DIR__ . '/TestAsset/error');
+        $namespace = 'error';
+        $template->addPath($path, $namespace);
+
+        $middleware = new HttpExceptionMiddleware();
+        $middleware->setTemplate($template);
+
+        $stack = [
+            $middleware,
+            function () use ($message): ResponseInterface {
+                throw new HttpException\BadRequestException($message);
+            },
+        ];
+
+        $response = Dispatcher::run($stack);
+        $contents = $response->getBody()->getContents();
+        $result   = json_decode($contents);
+
+        $this->verifyEntity($result->entity, $message);
+        $this->verifyException($result->exception, $message);
+    }
+
+    private function verifyEntity(stdClass $entity, string $message): void
+    {
+        unset($message);
+
+        $expected = HttpStatus::STATUS_BAD_REQUEST;
+        $this->assertSame($expected, $entity->statusCode);
+
+        $expected = 'Bad Request';
+        $this->assertSame($expected, $entity->name);
+
+        $expected = 'The request cannot be fulfilled due to bad syntax.';
+        $this->assertSame($expected, $entity->phrase);
+
+        $expected = 'Ctw\\Http\\HttpException\\BadRequestException';
+        $this->assertSame($expected, $entity->exception);
+
+        $expected = 'https://httpstatuses.com/400';
+        $this->assertSame($expected, $entity->url);
+    }
+
+    private function verifyException(stdClass $exception, string $message): void
+    {
+        $expected = HttpStatus::STATUS_BAD_REQUEST;
+        $this->assertSame($expected, $exception->statusCode);
+
+        $expected = $message;
+        $this->assertSame($expected, $exception->message);
     }
 }
